85113e577340171f02d88f3532dc4bb6c8334eab
seanwascoding <a225108347171@gmail.com>
2023-06-11 21:51:39 +0800

    新增了怪物扣血同步(需要加入互斥鎖避免)

--- database.sql
+++ database.sql
@@ -1,21 +1,20 @@
 /* Create */
-CREATE TABLE account_game (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+USE test;
+CREATE TABLE Team (
   username VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL,
-  /* email VARCHAR(100) NOT NULL, */
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  room_key VARCHAR(50) NOT NULL
 );
+
 SHOW TABLES;
 DESCRIBE account;
 
 /** Search */
 USE test;
-SELECT * FROM account_game;
+SELECT * FROM Team;
 
 /** Insert */
-INSERT INTO account (username, password)
-VALUES ('test', 'password');
+INSERT INTO Team (username, room_key)
+VALUES ('sean', 'test');
        
 /* Delete */
-DELETE FROM account;
+DELETE FROM Team;
--- index.js
+++ index.js
@@ -4,145 +4,8 @@ const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const app = express()
 
-/**  setup  */
-
-//* 解析
+/** 解析 */
 app.use(bodyParser.json())
-
-//* open server
-const PORT = 8080
-const server = app.listen(PORT, () => console.log(`Server open on ${PORT}`))
-
-//* WebSocket
-let i = 0
-let host = "";
-const json_temp = {}
-const json_temp_host = {}
-const json_temp_state = {}
-const json_temp_start = {}
-const map = new Map()
-const wss = new socketserver.Server({ server })
-wss.on('connection', ws => {
-    //! working to connected
-    console.log("working to connected")
-    map.set(ws, "")
-    json_temp.signal = "connecting"
-    ws.send(JSON.stringify(json_temp))
-
-    //! message
-    ws.on('message', (message) => {
-        const message_temp = JSON.parse(message)
-        console.log("Message from client:", message_temp)
-        //! create party
-        if (Object.keys(message_temp)[0] == '0') {
-            console.log("keywords send successfully")
-            //? team group create
-            map.set(ws, message_temp[Object.keys(message_temp)[0]])
-            console.log("keys:", map.get(ws))
-            //? send data (host)
-            json_temp.signal = message_temp[Object.keys(message_temp)[0]]
-            host = message_temp["name"]
-            console.log("host:", host)
-            ws.send(JSON.stringify(json_temp))
-        }
-        //! join party
-        else if (Object.keys(message_temp)[0] == '1') {
-            console.log("working to join party")
-            json_temp.signal = "working to join party"
-            json_temp_host.signal = "name from client"
-            //! send name to host
-            wss.clients.forEach((client) => {
-                if (client !== ws && client.readyState === socketserver.OPEN) {
-                    if (message_temp[Object.keys(message_temp)[0]] == map.get(client)) {
-                        //? client data
-                        json_temp.user = message_temp['name']
-                        json_temp.state = message_temp['state']
-                        json_temp.host = host
-
-                        //? team group
-                        map.set(ws, client)
-
-                        //? send data (client)
-                        ws.send(JSON.stringify(json_temp))
-
-                        //? client data
-                        console.log("(client)joining:", json_temp_host)
-
-                        //? host data
-                        json_temp_host.user = message_temp['name']
-                        json_temp_host.state = message_temp['state']
-
-                        //? send data (host)
-                        client.send(JSON.stringify(json_temp_host))
-
-                        //? host data
-                        console.log("(client)joining:", json_temp_host)
-
-                        i += 1
-                    }
-                }
-            });
-            //! identity 
-            if (i == 1) {
-                i = 0
-            }
-            else {
-                console.log("error")
-                json_temp.signal = "error"
-                ws.send(JSON.stringify(json_temp))
-            }
-        }
-        //! client ready
-        else if (Object.keys(message_temp)[0] == '2') {
-            try{
-                json_temp_state.signal = "ready"
-                json_temp_state.state = message_temp['2']
-                json_temp_state.user = message_temp['name']
-                map.get(ws).send(JSON.stringify(json_temp_state))
-            }catch(err){
-                console.log("client ready:", err)
-            }
-            
-        }
-        //! host start the game
-        else if (Object.keys(message_temp)[0] == '3') {
-            json_temp_start.signal="start"
-            map.forEach((value, key)=>{
-                if(key !== ws && key.readyState === socketserver.OPEN){
-                    if(value === ws){
-                        key.send(JSON.stringify(json_temp_start))
-                    }
-                }
-            })
-        }
-        //! exception
-        else {
-            console.log("error send message")
-            json_temp.signal = "error send message"
-            ws.send(JSON.stringify(json_temp))
-        }
-    })
-    //! disconnected
-    ws.on('close', () => {
-        try {
-            if (map.get(ws) !== "" && map.get(ws) !== undefined) {
-                if (map.get(ws).readyState === socketserver.OPEN) {
-                    console.log("")
-                }
-                else {
-                    console.log("error control", map.get(ws), "test")
-                }
-            }
-            else {
-                console.log("no pair group")
-            }
-        } catch (err) {
-            console.log(err)
-        }
-        map.delete(ws)
-        console.log("client disconnected")
-    })
-})
 
 /** database */
 
@@ -206,6 +69,226 @@ app.post('/registe', (req, res) => {
     })
 })
 
-/**  */
+/**  setup  */
 
-//* test_5
+//* open server
+const PORT = 8080
+const server = app.listen(PORT, () => console.log(`Server open on ${PORT}`))
+
+//* WebSocket
+let i = 0  //? corrected to true & false
+let monster = 1000
+let host = "";
+const json_temp = {}
+const json_temp_host = {}
+const json_temp_state = {}
+const json_temp_start = {}
+const json_temp_monster = {}
+const json_temp_loading = {}
+const map = new Map() //? team group (1st)
+const map_2 = new Map() //? team group (2st)
+const wss = new socketserver.Server({ server })
+wss.on('connection', ws => {
+    //! working to connected
+    console.log("working to connected")
+    map.set(ws, "")
+    json_temp.signal = "connecting"
+    ws.send(JSON.stringify(json_temp))
+
+    //! message
+    ws.on('message', (message) => {
+        const message_temp = JSON.parse(message)
+        console.log("Message from client:", message_temp)
+        //! create party
+        if (Object.keys(message_temp)[0] == '0') {
+
+            console.log("keywords send successfully")
+
+            //? team group create
+            map.set(ws, message_temp[Object.keys(message_temp)[0]])
+            console.log("keys:", message_temp['0'])
+
+            //? 
+            const name = message_temp['name']
+            host = message_temp['name']
+            pool.query('SELECT * FROM Team WHERE username = ?', [name], (err, result) => {
+                if (err) {
+                    console.log(err)
+                    ws.send(err)
+                }
+                else if (result) {
+                    //?
+                    console.log("exist")
+
+                    //? update
+                    pool.query('UPDATE Team SET room_key = ? WHERE username = ?', [message_temp['0'], name], (err, result) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        else {
+                            console.log("work to update")
+                            message_temp.signal = "successful create"
+                            ws.send(JSON.stringify(message_temp))
+                        }
+                    })
+                }
+                else {
+                    //?
+                    pool.query('INSERT INTO Team (username, room_key) VALUES (?, ?)', [name, message_temp['0']], (err) => {
+                        if (err) {
+                            console.log(err)
+                            message_temp.signal = "error"
+                            ws.send(JSON.stringify(message_temp))
+                        }
+                        else if (result) {
+                            console.log("successful")
+                            message_temp.signal = "successful create"
+                            ws.send(JSON.stringify(message_temp))
+                        }
+                    })
+                }
+                return
+            })
+        }
+        //! join party
+        else if (Object.keys(message_temp)[0] == '1') {
+            console.log("working to join party")
+            json_temp.signal = "working to join party"
+            json_temp_host.signal = "name from client"
+            //! send name to host
+            wss.clients.forEach((client) => {
+                if (client !== ws && client.readyState === socketserver.OPEN) {
+                    if (message_temp[Object.keys(message_temp)[0]] == map.get(client)) {
+                        //? client data
+                        json_temp.user = message_temp['name']
+                        json_temp.state = message_temp['state']
+                        json_temp.host = host
+
+                        //? team group
+                        map.set(ws, client)
+
+                        //? send data (client)
+                        ws.send(JSON.stringify(json_temp))
+
+                        //? client data
+                        console.log("(client)joining:", json_temp_host)
+
+                        //? host data
+                        json_temp_host.user = message_temp['name']
+                        json_temp_host.state = message_temp['state']
+
+                        //? send data (host)
+                        client.send(JSON.stringify(json_temp_host))
+
+                        //? host data
+                        console.log("(client)joining:", json_temp_host)
+
+                        i += 1
+                    }
+                }
+            });
+            //! identity 
+            if (i == 1) {
+                i = 0
+            }
+            else {
+                console.log("error")
+                json_temp.signal = "error"
+                ws.send(JSON.stringify(json_temp))
+            }
+        }
+        //! client ready
+        else if (Object.keys(message_temp)[0] == '2') {
+            try {
+                json_temp_state.signal = "ready"
+                json_temp_state.state = message_temp['2']
+                json_temp_state.user = message_temp['name']
+                map.get(ws).send(JSON.stringify(json_temp_state))
+            } catch (err) {
+                console.log("client ready:", err)
+            }
+
+        }
+        //! host start the game
+        else if (Object.keys(message_temp)[0] == '3') {
+            json_temp_start.signal = "start"
+            map.forEach((value, key) => {
+                if (key !== ws && key.readyState === socketserver.OPEN) {
+                    if (value === ws) {
+                        monster = 100
+                        key.send(JSON.stringify(json_temp_start))
+                    }
+                }
+            })
+        }
+        //! moster blood
+        else if (Object.keys(message_temp)[0] == '4') {
+            if (message_temp['4'] == "attack") {
+                monster -= parseFloat(message_temp['value'])
+                console.log("monster blood:", monster)
+                json_temp_monster.signal = "damage"
+                json_temp_monster.value = monster
+                ws.send(JSON.stringify(json_temp_monster))
+                wss.clients.forEach((client) => {
+                    if (client !== ws && client.readyState === socketserver.OPEN && map_2[client] == message_temp['key']) {
+                        client.send(JSON.stringify(json_temp_monster))
+                    }
+                })
+            }
+            else if (message_temp['4'] == "defend") {
+
+            }
+            else {
+                console.log("4:error")
+            }
+        }
+        //! replace
+        else if (Object.keys(message_temp)[0] == '5') {
+            //?
+            const name = message_temp['name']
+            // ws.send(JSON.stringify(message_temp))
+            pool.query('SELECT * FROM Team WHERE username = ? AND room_key = ?', [name, message_temp['key']], (err, result) => {
+                if (err) {
+                    console.log(err)
+                }
+                else if (result.length > 0) {
+                    map_2.set(ws, message_temp['key'])
+                    json_temp_loading.signal = "loading"
+                    ws.send(JSON.stringify(json_temp_loading))
+                }
+                else {
+                    console.log("error")
+                }
+            })
+        }
+        //! exception
+        else {
+            console.log("error send message")
+            json_temp.signal = "error send message"
+            ws.send(JSON.stringify(json_temp))
+        }
+    })
+    //! disconnected
+    ws.on('close', () => {
+        try {
+            if (map.get(ws) !== "" && map.get(ws) !== undefined) {
+                if (map.get(ws).readyState === socketserver.OPEN) {
+                    console.log("")
+                }
+                else {
+                    console.log("error control")
+                }
+            }
+            else {
+                console.log("no pair group")
+            }
+        } catch (err) {
+            console.log(err)
+        }
+        map.delete(ws)
+        console.log("client disconnected")
+
+        //? TO DO
+
+    })
+})
